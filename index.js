c574a2533bf03af9cd184437add7aa76c32aa75a
tom-costa <tomcosta@hotmail.co.uk>
2021-11-23 08:58:14 +0000

    Updates prior to submission

---
+++ index.js
@@ -0,0 +1,32 @@
+// Initialising variables with all the 'requires'
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+const app = express()
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  res.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT || 5000);
